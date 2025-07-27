5bbb83a5202f3b7c38086d18b08b0fc921d2d1d6
Mobamedloutfy <mohamedloutfy787@gmail.com>
2025-07-27 03:11:02 +0300

    Add/Update design tokens: tokens.dart

---
+++ design-tokens/tokens.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class DesignTokens {
+// =======================
+  // 🟦 Spacing (Sizing)
+  // =======================
+static const double spacingHuge = 134.0;
+static const double spacingXXL = 32.0;
+static const double spacingXl = 24.0;
+static const double spacingL = 16.0;
+static const double spacingMD = 12.0;
+static const double spacingSM = 8.0;
+static const double spacingXS = 4.0;
+static const double spacingXXS = 2.0;
+static const double spacingSm = 1.0;
+static const double spacingMd = 1.5.0;
+
+  // =======================
+  // 🟪 Radius (Border Radius)
+  // =======================
+static const double radiusXs = 4.0;
+static const double radiusSm = 8.0;
+static const double radiusMd = 12.0;
+
+  // =======================
+  // 🎨 Colors (Light Theme)
+  // =======================
+static const Color colorPrimary = Color(0xFF7300FF);
+static const Color colorPrimaryHover = Color(0xFF6800E6);
+static const Color colorPrimaryActive = Color(0xFF5C00CC);
+static const Color colorPrimaryContainer = Color(0xFFF1E6FF);
+static const Color colorPrimaryContainerHover = Color(0xFFEAD9FF);
+static const Color colorPrimaryContainerActive = Color(0xFFD4B0FF);
+static const Color colorSecondary = Color(0xFF5600BF);
+static const Color colorSecondaryHover = Color(0xFF450099);
+static const Color colorSecondaryActive = Color(0xFF340073);
+static const Color colorTextPrimary = Color(0xFF1E1E1E);
+static const Color colorTextSecondary = Color(0xFF666666);
+static const Color colorTextBody = Color(0xFF8C8C8C);
+static const Color colorSurface = Color(0xFFF8F7F4);
+static const double sizinglg = 404.0;
+static const double sizingMD = 201.0;
+static const double fontSizeXS = 10.0;
+static const double fontSize-S = 12.0;
+static const double fontSizeBody = 14.0;
+static const double fontSizeButton = 16.0;
+static const double fontSizeSubHeading = 20.0;
+static const double fontSizeHeading = 24.0;
+static const double fontSizeExtraHeading = 32.0;
+static const double reduisLg = 16.0;
+static const double reduisXL = 24.0;
+static const double shadowOpacity = 0.5.0;
+static const double primaryFontFamily = "Nunito";
+static const double latterSpacing = 0.0;
+static const double shadowColorBlack = "[object Object]";
+static const double shaowColorGray = "[object Object]";
+static const double fontWeight = "Regular";
+static const double fontWaight = "Medium";
+static const double lg = 2.0;
+static const double paragraphSpacing = -2.0;
+static const double lineHeight = 14.0;
+}
