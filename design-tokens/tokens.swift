57a4db486ff3d8c76c64189c1d5ccbe982e58381
Mobamedloutfy <mohamedloutfy787@gmail.com>
2025-07-27 03:11:00 +0300

    Add/Update design tokens: tokens.swift

---
+++ design-tokens/tokens.swift
@@ -0,0 +1,65 @@
+import SwiftUI
+
+struct DesignTokens {
+// Colors
+static let primaryColor = Color(hex: "#7300ff")
+static let primaryHover = Color(hex: "#6800e6")
+static let primaryActive = Color(hex: "#5c00cc")
+static let primaryLight = Color(hex: "#f1e6ff")
+static let primaryLitghtHover = Color(hex: "#ead9ff")
+static let primaryLightActive = Color(hex: "#d4b0ff")
+static let dark = Color(hex: "#5600bf")
+static let darkHover = Color(hex: "#450099")
+static let darkActive = Color(hex: "#340073")
+static let primaryTextColor = Color(hex: "#1e1e1e")
+static let secandryTextColor = Color(hex: "#666666")
+static let bodyTextColor = Color(hex: "#8c8c8c")
+static let cardColor = Color(hex: "#f8f7f4")
+
+    // Spacing
+static let spacingXXL: CGFloat = 32
+static let spacingXl: CGFloat = 24
+static let spacingL: CGFloat = 16
+static let spacingMD: CGFloat = 12
+static let spacingSM: CGFloat = 8
+static let spacingXS: CGFloat = 4
+static let spacingXXS: CGFloat = 2
+
+    // Font Sizes
+static let fontSizeXS: CGFloat = 10
+static let fontSize-S: CGFloat = 12
+static let fontSizeBody: CGFloat = 14
+static let fontSizeButton: CGFloat = 16
+static let fontSizeSubHeading: CGFloat = 20
+static let fontSizeHeading: CGFloat = 24
+static let fontSizeExtraHeading: CGFloat = 32
+
+    // Font Weights
+static let fontWeight = Font.Weight.regular
+static let fontWaight = Font.Weight.medium
+
+    // Font Families
+static let primaryFontFamily: String = "Nunito"
+static let shadowColorBlack: String = "[object Object]"
+static let shaowColorGray: String = "[object Object]"
+
+    // Shadows
+static let shadowOpacity: CGFloat = 0.5
+
+
+    // Other
+static let sizinglg: CGFloat = 404
+static let sizingMD: CGFloat = 201
+static let sizingSm: CGFloat = 134
+static let rediusXS: CGFloat = 4
+static let rediusSM: CGFloat = 8
+static let rediusMD: CGFloat = 12
+static let reduisLg: CGFloat = 16
+static let reduisXL: CGFloat = 24
+static let latterSpacing: CGFloat = 0
+static let sm: CGFloat = 1
+static let md: CGFloat = 1.5
+static let lg: CGFloat = 2
+static let paragraphSpacing: CGFloat = -2
+static let lineHeight: CGFloat = 14
+}
