af52a89b31a298616be33304fb198d8cd4187abf
Mobamedloutfy <mohamedloutfy787@gmail.com>
2025-07-27 03:11:01 +0300

    Add/Update design tokens: tokens.kt

---
+++ design-tokens/tokens.kt
@@ -0,0 +1,60 @@
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+object DesignTokens {
+
+// Spacing Tokens (Material spacing scale)
+val spacingLg = 134.dp
+val spacingXXL = 32.dp
+val spacingXl = 24.dp
+val spacingL = 16.dp
+val spacingMD = 12.dp
+val spacingSM = 8.dp
+val spacingXS = 4.dp
+val spacingXXS = 2.dp
+val spacingSm = 1.dp
+val spacingMd = 1.5.dp
+
+    // Radius (Shape)
+val radiusXs = 4.dp
+val radiusSm = 8.dp
+val radiusMd = 12.dp
+
+    // Material Design 3 - Light Theme Primary Colors
+val primaryLight = Color(0xFF7300FF)
+val primaryContainerLight = Color(0xFF5C00CC)
+val primaryContainerLight = Color(0xFFF1E6FF)
+val primaryLitghtHover = Color(0xFFEAD9FF)
+val secondaryLight = Color(0xFF5600BF)
+val onSurfaceLight = Color(0xFF1E1E1E)
+val surfaceLight = Color(0xFFF8F7F4)
+
+    // Material Design 3 - Light Theme Secondary & Text Colors
+val onPrimaryLight = Color(0xFF6800E6)
+val onPrimaryContainerLight = Color(0xFFD4B0FF)
+val onSecondaryLight = Color(0xFF450099)
+val secondaryContainerLight = Color(0xFF340073)
+val onSurfaceVariantLight = Color(0xFF666666)
+val outlineLight = Color(0xFF8C8C8C)
+val sizinglg = 404.dp
+val sizingMD = 201.dp
+val fontSizeXS = 10.dp
+val fontSize-S = 12.dp
+val fontSizeBody = 14.dp
+val fontSizeButton = 16.dp
+val fontSizeSubHeading = 20.dp
+val fontSizeHeading = 24.dp
+val fontSizeExtraHeading = 32.dp
+val reduisLg = 16.dp
+val reduisXL = 24.dp
+val shadowOpacity = 0.5.dp
+val primaryFontFamily = Nunito.dp
+val latterSpacing = 0.dp
+val shadowColorBlack = [object Object].dp
+val shaowColorGray = [object Object].dp
+val fontWeight = Regular.dp
+val fontWaight = Medium.dp
+val lg = 2.dp
+val paragraphSpacing = -2.dp
+val lineHeight = 14.dp
+}
